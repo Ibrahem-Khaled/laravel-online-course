790a51810c7988c1f846464a09efa680b1546320
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-12-10 17:35:50 +0200

    change background color

--- resources/views/homeComponents/home/course-card.blade.php
+++ resources/views/homeComponents/home/course-card.blade.php
@@ -2,7 +2,7 @@
     .course-card {
         width: 100%;
         max-width: 286px;
-        background-color: #145466;
+        background-color: #035971;
         border-radius: 10px;
         position: relative;
         overflow: hidden;
