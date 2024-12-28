213c6e8941ec5492716b774533cd75fa078265f1
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-12-28 02:34:27 +0200

    fix bug from course section

--- resources/views/user-section.blade.php
+++ resources/views/user-section.blade.php
@@ -196,8 +196,7 @@
         <div class="tab-pane fade" id="sources" role="tabpanel" aria-labelledby="sources-tab">
             <section class="mt-5" style="text-align: right; width: 90%; margin: 10px auto;">
                 <h2 class="info-header">المنهج والدورات</h2>
-                <div
-                    style="display: flex; flex-wrap: wrap; justify-content: space-around; direction: rtl; width: 90%; margin:auto;">
+                <div class="row" style="direction: rtl;">
                     @foreach ($sectionCourses as $course)
                         @include('homeComponents.home.course-card', ['course' => $course])
                     @endforeach
