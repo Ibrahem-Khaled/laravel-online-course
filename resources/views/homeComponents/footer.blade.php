89779df84f8cca1c0443888070858444c2222620
Ibrahem-Khaled <xx.vvv48@gmail.com>
2025-02-05 08:00:25 +0200

    improve footer ui

--- resources/views/homeComponents/footer.blade.php
+++ resources/views/homeComponents/footer.blade.php
@@ -5,7 +5,6 @@
         padding: 4rem 0 0;
         position: relative;
         overflow: hidden;
-        font-family: 'Poppins', sans-serif;
     }
 
     .footer::before {
@@ -19,10 +18,12 @@
 
     .footer-section {
         margin-bottom: 2.5rem;
+        align-items: center;
+        display: flex;
     }
 
     .footer h6 {
-        color: #2ad4d9;
+        color: #ed6b2f;
         font-weight: 600;
         margin-bottom: 1.5rem;
         position: relative;
@@ -36,7 +37,7 @@
         left: 0;
         width: 40px;
         height: 2px;
-        background: #2ad4d9;
+        background: #ed6b2f;
     }
 
     .footer-links {
@@ -67,7 +68,7 @@
         left: 0;
         width: 0;
         height: 1px;
-        background: #2ad4d9;
+        background: #ed6b2f;
         transition: width 0.3s ease;
     }
 
@@ -104,7 +105,7 @@
     }
 
     .social-icon:hover {
-        background: #2ad4d9;
+        background: #ed6b2f;
         transform: translateY(-3px);
     }
 
@@ -164,19 +165,19 @@
                 <img src="{{ asset('assets/img/logo-ct-dark.png') }}" alt="Al-Ruad" class="footer-logo">
                 <div class="contact-details">
                     <p class="d-flex align-items-center justify-content-end gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#2ad4d9">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#ed6b2f">
                             <path d="M20 22.621l-3.521-6.795c-.008.004-1.974.97-2.064 1.011-2.24 1.086-6.799-7.82-4.609-8.994l2.083-1.026-3.493-6.817-2.106 1.039c-7.202 3.755 4.233 25.982 11.6 22.615.121-.055 2.102-1.029 2.11-1.033z"/>
                         </svg>
                         <a href="https://wa.me/966581870300" target="_blank" class="text-light">+966 58 187 0300</a>
                     </p>
                     <p class="d-flex align-items-center justify-content-end gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#2ad4d9">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#ed6b2f">
                             <path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/>
                         </svg>
                         المملكة العربية السعودية
                     </p>
                     <p class="d-flex align-items-center justify-content-end gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#2ad4d9">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#ed6b2f">
                             <path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725"/>
                         </svg>
                         <a href="mailto:info@alsorayaiacademy.com" class="text-light">info@alsorayaiacademy.com</a>
