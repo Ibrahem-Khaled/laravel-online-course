30303d97a9c4308cd68bfc957ce19e17c0766aed
Ibrahem-Khaled <xx.vvv48@gmail.com>
2025-02-07 12:03:57 +0200

    change image varible

--- resources/views/homeComponents/header.blade.php
+++ resources/views/homeComponents/header.blade.php
@@ -176,7 +176,7 @@
                     <!-- حساب المستخدم -->
                     <div class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" data-toggle="dropdown">
-                            <img src="{{ Auth::user()->image }}" class="rounded-circle user-avatar"
+                            <img src="{{ Auth::user()->profile_image }}" class="rounded-circle user-avatar"
                                 style="width:35px;height:35px;object-fit:cover;margin-left:10px;">
                             <span>{{ Auth::user()->name }}</span>
                         </a>
